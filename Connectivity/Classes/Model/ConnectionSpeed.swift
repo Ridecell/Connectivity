29c3712ac2de593657715e6662226b5bde2334e4
ayush-ridecell <ayush@ridecell.com>
2023-08-07 14:05:26 +0530

    CAR-23516: Adds disconnected case to ConnectionSpeed enum

--- Connectivity/Classes/Model/ConnectionSpeed.swift
+++ Connectivity/Classes/Model/ConnectionSpeed.swift
@@ -12,16 +12,19 @@ public enum ConnectionSpeed {
     case fast
     case slow
     case poor
+    case disconnected
     case unknown
     
     static func estimatedSpeed(_ timeTaken: TimeInterval) -> Self {
         switch timeTaken {
-        case 0.0...0.8:
+        case 0.0...0.5:
             return .fast
-        case 0.8...1.5:
+        case 0.5...1.0:
             return .slow
-        case 1.5...5.0:
+        case 1.0...5.0:
             return .poor
+        case 5.0...:
+            return .disconnected
         default:
             return .unknown
         }
